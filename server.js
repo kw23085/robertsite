732a9bf89fa0921ed18e2f47c9fed8852a54566f
Kevin Wang <kw1017@gmail.com>
2018-04-20 19:58:54 -0700

    set up server connect mongoose

---
+++ server.js
@@ -0,0 +1,40 @@
+const
+  express = require('express'),
+  app = express(),
+  ejsLayouts = require('express-ejs-layouts'),
+  mongoose = require('mongoose'),
+  logger = require('morgan'),
+  bodyParser = require('body-parser')
+
+
+// Environment port(online)
+const
+port = process.env.PORT || 3000,
+mongoConnectionString = process.env.MONGODB_URI || 'mongodb://localhost/robertsite'
+
+// Mongoose Connection
+mongoose.connect(mongoConnectionString, (err) => {
+  console.log(err || "Connected to MongoDB!🐲")
+})
+
+// Middleware
+app.use(logger('dev'))
+app.use(bodyParser.json())
+
+// ejs Configuration
+app.set('view engine', 'ejs')
+app.use(ejsLayouts)
+app.use(express.static(`${__dirname}`))
+
+
+// Root route
+app.get('/', (req, res) => {
+  res.json("root route bro!")
+})
+
+
+
+// Port
+app.listen(port,(err) => {
+  console.log(err || "Server runing on port " + port + "!🐉")
+})
